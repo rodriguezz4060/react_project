64cec3379dc6905be933bf04116b5a63be5fc9ca
Rodriguezz <rodriguezz4060@gmail.com>
2022-03-19 16:50:16 +0200

    start projekt

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://yt3.ggpht.com/ytc/AKedOLS1AZhEVoT69mDznUiqA5tTkS4D47iqYSE7eYpNCg=s48-c-k-c0x00ffffff-no-rj' />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' />
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            My post
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
